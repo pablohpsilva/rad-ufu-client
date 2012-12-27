edf1c2b1c06f5b10cca0f51de6ae9dde133a64ea
Yassin <yassin791@gmail.com>
2012-12-27 18:23:14 -0200

    teste com criação de modelos

--- js/main.js
+++ js/main.js
@@ -31,16 +31,59 @@ require.config({
 require([
 
     "app",
-    "collections/categoria"
+    "collections/categoria",
+    "models/professor",
+    "models/tipoAtividade",
+    "models/multiplicador",
+    "models/comprovante",
+    "models/atividade"
 
-    ], function(App, CategoriaCollection) {
+    ], function(App, CategoriaCollection, Professor, Tipo, Mult, Comp, Ativ) {
 
-        categorias = CategoriaCollection;
+        var categorias = CategoriaCollection;
         categorias.create({id:1, nome:"Ensino"});
         categorias.create({id:2, nome:"Orientação"});
         categorias.create({id:3, nome:"Produção"});
         categorias.create({id:4, nome:"Pesquisa"});
 
-        new App.init();
+        var tipo = new Tipo({
+            id:1,
+            codigo:1,
+            categoria: categorias.get(1),
+            descricao: "blablabla",
+            pontuacao: 2,
+            limitePontos: 80,
+            pontuacaoRef: 1,
+        });
+
+        tipo.get("multiplicadores").create({
+            id:1,
+            nome:"mult",
+            valor:10,
+            limite:11
+        });
+
+        tipo.get("multiplicadores").create({
+            id:2,
+            nome:"multiplic",
+            valor:5,
+            limite:10
+        });
+
+        var ativ = new Ativ({
+            id:1,
+            descricao:"",
+            inicio: new Date(),
+            fim: new Date(),
+            tipo:tipo
+        });
+
+        ativ.get("comprovantes").create({id:1,arquivo:"the X files"});
+
+
+        app = App;
+        app.loggedProf = new Professor({id:1});
+        app.loggedProf.get("atividades").add(ativ);
+        app.init();
 
 });
