34c24a701b44212512a81a7ace1805a818ff64a1
Yassin <yassin791@gmail.com>
2013-01-14 19:36:32 -0200

    fix erro na validação de comprovantes

--- js/models/atividade.js
+++ js/models/atividade.js
@@ -22,9 +22,9 @@ define([
             },
 
             validate : function(attrs) {
-                if(attrs.inicio === null|| attrs.fim === null)
+                if(attrs.inicio === null || attrs.fim === null)
                     return "data de início ou fim não atribuídas a atividade";
-                if(attrs.comprovante === null)
+                if(attrs.comprovantes === null)
                     return "comprovante não anexado a atividade";
                 if(attrs.tipo === null)
                     return "atividade sem tipo";
