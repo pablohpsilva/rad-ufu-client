de624541ec8a955f1fba80531f5f7a778723c3de
Yassin <yassin791@gmail.com>
2013-01-15 20:39:34 -0200

    listagem das catagorias existentes

--- js/views/atividades/cadastro.js
+++ js/views/atividades/cadastro.js
@@ -1,8 +1,9 @@
 define([
 
+    "collections/categoria",
     "../../../components/require/text!templates/atividades/cadastro.html",
 
-    ], function(cadastroAtivTpl) {
+    ], function(catCollection, cadastroAtivTpl) {
 
         var CadastroAtividadeView = Backbone.View.extend({
 
@@ -10,7 +11,12 @@ define([
 
             render : function() {
 
-                this.$el.html(_.template(cadastroAtivTpl, {categoria:this.model}));
+                var data = {
+                    categorias : catCollection.toJSON(),
+                    categoria  : this.model,
+                };
+
+                this.$el.html(_.template(cadastroAtivTpl, data));
 
                 return this;
 
