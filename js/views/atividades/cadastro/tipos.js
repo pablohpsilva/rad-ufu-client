56d3750f5e89e776a6a3b39ac040066e8e0e32a7
Yassin <yassin791@gmail.com>
2013-01-17 00:52:20 -0200

    subView descrição

--- js/views/atividades/cadastro/tipos.js
+++ js/views/atividades/cadastro/tipos.js
@@ -1,28 +1,60 @@
 define([
 
     "collections/tipo",
+    "views/atividades/cadastro/descricao",
     "../../../../components/require/text!templates/atividades/cadastro/tipos.html"
 
-    ],  function(tCollection, tiposTpl) {
+    ],  function(tCollection, DescricaoView, tiposTpl) {
 
         var CategoriasView = Backbone.View.extend({
 
+            subViews : {
+                descricao : new DescricaoView()
+            },
 
+            initialize : function() {
+                this.on("close", this.limpaSubviews, this);
+                this.on("tipoSelected", this.tipoSelected, this);
+            },
 
-            render : function(data) {
+            tipoSelected : function() {
+                var tipoCodigo = $("#categoria-selector").val().toLowerCase();
+                this.renderDescricao(tipoCodigo);
+            },
+
+            render : function(catSelecionada) {
 
                 var tiposDaCategoria =
                     _.chain(tCollection.toJSON())
                      .filter(function(t){
-                        return t.categoria.get("nome").toLowerCase() === data.categoria
-                            || t.categoria.get("id") == data.categoria;
+                        return t.categoria.get("nome").toLowerCase() === catSelecionada
+                            || t.categoria.get("id") == catSelecionada;
                      })
                      .value();
 
-                data.tipos = tiposDaCategoria;
-                this.$el.html(_.template(tiposTpl, data));
+                if (_.isEmpty(tiposDaCategoria)) {
+                    this.close();
+                } else {
+                    this.$el.html(_.template(tiposTpl, {
+                        tipos : tiposDaCategoria
+                    }));
+
+                    this.trigger("tipoSelected");
+                }
 
                 return this;
+            },
+
+            renderDescricao : function(tipoCodigo) {
+                this.subViews.descricao
+                    .setElement($("#descricao-block"))
+                    .render(tipoCodigo);
+            },
+
+            limpaSubviews : function() {
+                _.each( _(this.subViews).values(), function (subView) {
+                   subView.close();
+                });
             }
         });
 
