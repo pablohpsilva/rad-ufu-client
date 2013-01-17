8ca976be307b8a26199a79cbaad92b7178183e76
Yassin <yassin791@gmail.com>
2013-01-17 00:45:05 -0200

    método close transferido para js/app

--- js/app.js
+++ js/app.js
@@ -9,6 +9,15 @@ define([
             init : function() {
 
                 new Router();
+
+                // Método adicionado Backbone.View para destruir as views, no final
+                // dispara o evento 'close', as views podem escutar este evento para
+                // executar rotinas de limpeza
+                Backbone.View.prototype.close = function() {
+                    this.$el.empty();
+                    this.stopListening();
+                    this.trigger("close");
+                };
             }
 
         };
--- js/views/manager.js
+++ js/views/manager.js
@@ -11,18 +11,6 @@ define([
 
             factory : new ViewFactory(),
 
-            initialize : function() {
-
-                // Método adicionado Backbone.View para destruir as views, no final
-                // dispara o evento 'close', as views podem escutar este evento para
-                // executar rotinas de limpeza
-                Backbone.View.prototype.close = function() {
-                    this.$el.empty();
-                    this.stopListening();
-                    this.trigger("close");
-                };
-            },
-
             // Destrói a view criada anteriormente, renderiza uma nova view e a
             // marca como view atual. As views são instânciadas passando 'options'
             // como parâmetro. Views estáticas (devem ser renderizdas apenas uma
