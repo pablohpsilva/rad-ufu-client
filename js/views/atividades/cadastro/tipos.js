b1860a078a36cb23e00d4833d21c9cd38aae3929
Yassin <yassin791@gmail.com>
2013-01-16 23:09:53 -0200

    passagem da logica do templete para a view

--- js/views/atividades/cadastro/tipos.js
+++ js/views/atividades/cadastro/tipos.js
@@ -7,9 +7,19 @@ define([
 
         var CategoriasView = Backbone.View.extend({
 
+
+
             render : function(data) {
 
-                data.tipos = tCollection.toJSON();
+                var tiposDaCategoria =
+                    _.chain(tCollection.toJSON())
+                     .filter(function(t){
+                        return t.categoria.get("nome").toLowerCase() === data.categoria
+                            || t.categoria.get("id") == data.categoria;
+                     })
+                     .value();
+
+                data.tipos = tiposDaCategoria;
                 this.$el.html(_.template(tiposTpl, data));
 
                 return this;
