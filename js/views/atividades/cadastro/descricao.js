dd51b1045eb028c1d267c3210222dd4ef31d5e0b
Yassin <yassin791@gmail.com>
2013-02-05 09:34:52 -0200

    view descrição da atividade

---
+++ js/views/atividades/cadastro/descricao.js
@@ -0,0 +1,25 @@
+define([
+
+    "collections/tipo",
+    "../../../../components/require/text!templates/atividades/cadastro/descricao.html"
+
+    ],  function(tCollection, descricaoTpl) {
+
+        var DescricaoView = Backbone.View.extend({
+
+            render : function(tipoCodigo) {
+                //console.log("render descricao tipo:", tipoCodigo);
+                var model = tCollection
+                        .where({codigo: +tipoCodigo})
+                        .pop();
+
+                var descricao = (model)? model.get("descricao") : "";
+
+                this.$el.html(_.template(descricaoTpl, {
+                    descricao : descricao
+                }));
+            }
+        });
+
+        return DescricaoView;
+});
