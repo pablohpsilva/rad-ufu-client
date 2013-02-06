08edff0726caf64b51cadf130ea37ffb21cad805
Yassin <yassin791@gmail.com>
2013-02-05 23:48:48 -0200

    modulo para alerts

---
+++ js/views/alert.js
@@ -0,0 +1,24 @@
+//
+// Módulo para renderização de alerts do bootstrap as options é um map {type, msg}
+// o 'type' é um tipo de alert do bootstrap e 'msg' é a mensagem a ser exibida
+//
+define([
+
+    "../../components/require/text!templates/alert.html"
+
+], function (alertTpl) {
+
+   var AlertView = Backbone.View.extend({
+        el : "#err",
+
+        render : function (options) {
+            var args = (!options) ? {} : options;
+            if(!args.type) args.type = "";
+
+            this.$el.html(_.template(alertTpl, args));
+        }
+   });
+
+   return AlertView;
+
+});
