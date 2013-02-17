65de002bc9e8c2293df7d24c6b44f027bfb4e0d9
Yassin <yassin791@gmail.com>
2013-02-17 10:18:44 -0300

    jshint

--- src/js/router.js
+++ src/js/router.js
@@ -24,57 +24,43 @@ define([
 
                 this.viewManager.show("navbar", {once:true});
 
-                this.on(
-                    "route:atividades"
-                    ,   function(categoria) {
-                            categoria = decodeURIComponent(categoria);
-                            this.viewManager.show("atividades", {model:categoria});
+                this.on("route:atividades", function (categoria) {
+                    categoria = decodeURIComponent(categoria);
+                    this.viewManager.show("atividades", {model:categoria});
                 });
 
-                this.on(
-                    "route:cadastrarAtividade"
-                    ,   function(categoria) {
-                            categoria = decodeURIComponent(categoria);
-                            this.viewManager.show("atividades:cadastro", {model:categoria});
+                this.on("route:cadastrarAtividade", function (categoria) {
+                    categoria = decodeURIComponent(categoria);
+                    this.viewManager.show("atividades:cadastro", {model:categoria});
                 });
 
 
                 // Redireciona para a route de atividades da primeira categoria na
                 // collection de categorias
-                this.on(
-                    "route:primeiraCategoria"
-                    ,   function() {
-                            var cat, categorias = categoriaCollection;
+                this.on("route:primeiraCategoria", function () {
+                    var cat, categorias = categoriaCollection;
 
-                            categorias.fetch();
-                            cat = encodeURIComponent(categorias.at(0).get("nome").toLowerCase());
+                    categorias.fetch();
+                    cat = encodeURIComponent(categorias.at(0).get("nome").toLowerCase());
 
-                            this.navigate("#/atividades/" + cat, {trigger:true, replace:true});
+                    this.navigate("#/atividades/" + cat, {trigger:true, replace:true});
                 });
 
-                this.on(
-                    "route:relatorio"
-                    ,   function(){
-                        this.viewManager.show("relatorio");
+                this.on("route:relatorio", function () {
+                    this.viewManager.show("relatorio");
                 });
 
-                this.on(
-                    "route:ajuda"
-                    ,   function(){
-                        this.viewManager.show("ajuda");
+                this.on("route:ajuda", function(){
+                    this.viewManager.show("ajuda");
                 });
 
-                this.on(
-                    "route:paginaInicial"
-                    ,   function(){
-                            this.viewManager.show("inicio");
+                this.on("route:paginaInicial", function(){
+                    this.viewManager.show("inicio");
                 });
 
-                this.on(
-                    "route:defaultAction"
-                    ,   function(actions){
-                            if (!actions) this.navigate("#/inicio");
-                                console.log('No route:', actions);
+                this.on("route:defaultAction", function(actions){
+                    if (!actions) this.navigate("#/inicio");
+                        console.log('No route:', actions);
                 });
 
                 Backbone.history.start();
