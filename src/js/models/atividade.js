aed52d747b1b57eb99156fe799dae9448501b892
Yassin <yassin791@gmail.com>
2013-03-04 21:23:35 -0300

    Property length of object [object Array] is not a function

--- src/js/models/atividade.js
+++ src/js/models/atividade.js
@@ -15,7 +15,7 @@ define([], function() {
             validate : function(attrs) {
                 if(attrs.inicio === "" || attrs.fim === "")
                     return "data de início ou fim não atribuídas a atividade";
-                if(attrs.comprovantes.length() === 0)
+                if(attrs.comprovantes.length === 0)
                     return "comprovante não anexado a atividade";
                 if(attrs.tipo === 0)
                     return "atividade sem tipo";
