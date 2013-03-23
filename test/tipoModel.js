555077dc2d757462e251829654093fa6b8c472af
Yassin <yassin791@gmail.com>
2013-03-23 20:37:51 -0300

    teste da função parse() do tipo

--- test/tipoModel.js
+++ test/tipoModel.js
@@ -1,8 +1,10 @@
 define([
 
-    "models/tipo"
+    "models/tipo",
+    "collections/categoria",
+    "collections/multiplicador"
 
-    ], function (Tipo) {
+    ], function (Tipo, categoriaCollection, multCollection) {
         describe("Tipo Model", function () {
             describe("Após inicialização (default)", function () {
                 beforeEach(function (done) {
@@ -31,6 +33,46 @@ define([
                 });
                 it("O id do multiplicador deve ser zero", function (done) {
                     this.tipo.get("categoria").should.equal(0);
+                    done();
+                });
+            });
+
+            describe('Interação com a API', function () {
+                beforeEach(function (done) {
+                    this.tipo = new Tipo();
+                    this.mCollection = multCollection;
+                    this.cCollection = categoriaCollection;
+                    done();
+                });
+                afterEach(function (done) {
+                    this.mCollection.reset();
+                    this.cCollection.reset();
+                    done();
+                });
+                it('Deve adicionar o comprovante e o multiplicador as respectivas collections e guardar apenas os ids destes', function (done) {
+                    var jsonResponseStub =
+                        {
+                            "categoria": {
+                                "id": 1,
+                                "nome": "Ensino"
+                            },
+                            "descricao": "Aula teórica ou prática para turmas adicionais da mesma disciplina, ministradapelo mesmo docente, e no mesmo Curso, enquadrada na Situação 2 (ver OBS.).",
+                            "id": 3,
+                            "limitePontos": null,
+                            "multiplicador": {
+                                "id": 1,
+                                "nome": "Aulas / Semana"
+                            },
+                            "pontuacao": 5,
+                            "pontuacaoRef": null
+                        };
+
+                    this.attrHash = this.tipo.parse(jsonResponseStub);
+
+                    this.attrHash.categoria.should.equal(1);
+                    this.attrHash.multiplicador.should.equal(1);
+                    this.mCollection.should.have.length(1);
+                    this.cCollection.should.have.length(1);
                     done();
                 });
             });
