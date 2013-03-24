efba546bfc33b02807bc3d8775ed0074d538dab6
Yassin <yassin791@gmail.com>
2013-03-23 23:16:38 -0300

    app agora é um backbone model

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,41 @@
 define([
 
-    "router"
+    "module",
+    "collections/comprovante",
+    "collections/atividade"
 
-    ], function(Router) {
+    ], function (module, comprovanteCollection, atividadeCollection) {
 
-        return {
-
-            init : function() {
-
-                // Método adicionado Backbone.View para destruir as views, no final
-                // dispara o evento 'close', as views podem escutar este evento para
-                // executar rotinas de limpeza
-                Backbone.View.prototype.close = function() {
-                    this.undelegateEvents();
-                    this.$el.empty();
-                    this.stopListening();
-                    this.trigger("close");
-                };
-
-                new Router();
-            }
-
+        // Método adicionado Backbone.View para destruir as views, no final
+        // dispara o evento 'close', as views podem escutar este evento para
+        // executar rotinas de limpeza
+        Backbone.View.prototype.close = function() {
+            this.undelegateEvents();
+            this.$el.empty();
+            this.stopListening();
+            this.trigger("close");
         };
 
+
+        var App = Backbone.Model.extend({
+
+            server: module.config().serverLocation,
+            api:    module.config().apiLocation,
+            user:   module.config().loggedUser,
+            isDemo: module.config().demo,
+
+            bootstrapCollections: function () {
+                if (!this.isDemo) {
+                    comprovanteCollection.url   = this.server + this.api + "comprovante";
+                    atividadeCollection.url     = this.server + this.api +
+                        "professor/" + this.user.id + "/atividade";
+                }
+            },
+
+            initialize: function  () {
+                this.bootstrapCollections();
+            }
+        });
+
+        return App;
 });
