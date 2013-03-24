73f36cc16c2aea9deb1d2e9514c6e4ec53bfd167
Yassin <yassin791@gmail.com>
2013-03-24 15:24:33 -0300

    teste atualizado

--- test/aplicacao.js
+++ test/aplicacao.js
@@ -2,20 +2,31 @@ define([
 
     "../src/js/app",
     "collections/comprovante",
-    "collections/atividade"
+    "collections/atividade",
+    "collections/categoria"
 
-    ], function (App, comprovanteCollection, atividadeCollection) {
+    ], function (App, comprovanteCollection, atividadeCollection, categoriaCollection) {
 
         describe('Aplicação', function () {
             beforeEach(function (done) {
                 this.app = new App();
                 this.cCollection = comprovanteCollection;
                 this.aCollection = atividadeCollection;
+                this.atividadeCollectionFetchStub = sinon.stub(atividadeCollection, "fetch")
+                    .returns("success");
+                this.categoriaCollectionFetchStub = sinon.stub(categoriaCollection, "fetch")
+                    .returns("success");
+                done();
+            });
+            afterEach(function (done) {
+                this.atividadeCollectionFetchStub.restore();
+                this.categoriaCollectionFetchStub.restore();
                 done();
             });
             it('Deve setar as urls da comprovanteCollection e da atividadeCollection', function (done) {
-                this.cCollection.url.should.equal("localhost/rad-ufu/api/comprovante");
-                this.aCollection.url.should.equal("localhost/rad-ufu/api/professor/1/atividade");
+                this.app.bootstrapCollections();
+                this.cCollection.url.should.equal("api/comprovante");
+                this.aCollection.url.should.equal("api/professor/1/atividade");
                 done();
             });
         });
