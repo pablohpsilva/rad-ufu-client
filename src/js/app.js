1d4ee5b016b0b149b66ed2098039424a50cdb302
Yassin <yassin791@gmail.com>
2013-03-24 16:47:19 -0300

    src/js/app.js

--- src/js/app.js
+++ src/js/app.js
@@ -20,16 +20,29 @@ define([
 
 
         var App = Backbone.Model.extend({
-            user:   module.config().loggedUser,
-            isDemo: module.config().demo,
+
+            user:       module.config().loggedUser,
+            isDemo:     module.config().demo,
+            atividades: module.config().atividades,
+            categorias: module.config().categorias,
 
             bootstrapCollections: function () {
+                var Atividade = atividadeCollection.model;
+
+                console.log("App > Inicializando collections");
+
                 categoriaCollection.url   = "api/categoria";
                 comprovanteCollection.url = "api/comprovante";
                 atividadeCollection.url   = "api/professor/" + this.user.id + "/atividade";
 
-                categoriaCollection.fetch();
-                atividadeCollection.fetch();
+                console.log("App > user: ", this.user);
+
+                _.each(this.atividades, function (atividadeServerJSON, i) {
+                    this.atividades[i] = Atividade.prototype.parse(atividadeServerJSON);
+                }, this);
+
+                categoriaCollection.reset(this.categorias);
+                atividadeCollection.reset(this.atividades);
             }
 
         });
