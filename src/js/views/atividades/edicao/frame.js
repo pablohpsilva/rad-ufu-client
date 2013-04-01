bbf93113ea99e69e7f83f98ed6a029320a22c4ec
Yassin <yassin791@gmail.com>
2013-04-01 13:12:00 -0300

    edição de atividades usando promises em vez de controle de fluxo explicito

--- src/js/views/atividades/edicao/frame.js
+++ src/js/views/atividades/edicao/frame.js
@@ -35,8 +35,6 @@ define([
 
                 this.listenTo(this.subViews.categorias, "change", this.renderTipos);
 
-                this.listenTo(this.model, "sync", this.atividadeAlterada);
-
                 //console.log(this.model);
             },
 
@@ -75,12 +73,53 @@ define([
 
                 atividade = this.preparaDados();
 
+                var alert = _.bind(this.alert, this);
+                var atividadeAttrHash     = _.omit(atividade, "toDestroy" ,"atuais", "err", "selecionados", "categoria");
+                var comprovantesToRm = atividade.toDestroy;
+
+                var Comprovante       = comprovanteCollection.model;
+                var comprovantesToAdd = _.map(atividade.selecionados, function (file) {
+                    var c = new Comprovante({ nome: file.name, arquivo: file, atividade: this.model.get("id")});
+                    c.urlRoot = comprovanteCollection.url;
+                    return c;
+                }, this);
+
                 if (!_.isEmpty(atividade.err)) {
+
                     _.each(atividade.err, function (errMsg) {
                         this.alert(errMsg, { type: "err" });
                     }, this);
+
                 } else {
 
+                    var atividadeValida = this.model.save(atividadeAttrHash, { wait: true });
+
+                    if (atividadeValida) {
+
+                        atividadeValida.then(function () {
+
+                            return $.when.apply(this, _.map(comprovantesToAdd, function (c) { return c.save(); }));
+
+                        }).then(function () {
+
+                            comprovanteCollection.add(comprovantesToAdd);
+                            _.each(comprovantesToAdd, function (c) { addToAtividade(c); });
+                            alert("Dados da atividade alterados", { type: "success" });
+
+                        }, function (jqXHR, textStatus, errorThrown) {
+                            _.each(comprovantesToAdd, function (c) { c.destroy(); });
+                        });
+
+                        _.each(comprovantesToRm, function (c) {
+                            c.destroy({ success: rmFromAtividade });
+                        });
+
+                    } else {
+                        console.log(this.model.validationError);
+                    }
+
+
+                    /*
                     // se existem arquivos comprovantes para excluir da atividade
                     if (!_.isEmpty(atividade.toDestroy)) {
                         _.each(atividade.toDestroy, function (c) {
@@ -101,12 +140,10 @@ define([
                     }
                     atividade = _.omit(atividade, "toDestroy" ,"atuais", "err", "selecionados", "categoria");
                     this.model.save(atividade);
+                    */
+
                 }
                 console.log(atividade);
-            },
-
-            atividadeAlterada: function () {
-                this.alert("Dados da atividade alterados", { type: "success" });
             }
         });
 
